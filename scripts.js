b43183a07eb88a293aaab169285cf622f19f0d57
Nicholas Braddock <nicholas.braddock@student.swic.edu>
2020-12-07 16:14:28 -0600

    Removed items pertaining to client number and added the display function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-import { processForm } from "./lib.js";
+import data from "./data.js";
+import { clients, createCurrentClients } from "./lib.js";
 const form = document.querySelector("form");
+
+const display = (clientData) => {
+  createCurrentClients(clientData);
+};
+
+display(data);
 
 form.addEventListener("submit", function () {
   event.preventDefault();
 
-  processForm(event.target.elements);
+  data.push(clients(event.target.elements));
+  display(data);
 });
