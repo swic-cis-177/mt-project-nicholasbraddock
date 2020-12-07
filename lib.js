f60b705401443f418074950cd4f8d81c38965975
Nicholas Braddock <nicholas.braddock@student.swic.edu>
2020-12-07 16:02:27 -0600

    Added the creation of the table function

--- lib.js
+++ lib.js
@@ -1,4 +1,7 @@
-export const processForm = (elements) =>
+const tbody = document.querySelector("tbody");
+const template = document.querySelector("template");
+
+const processForm = (elements) =>
   Array.from(elements)
     .filter(({ id }) => id)
     .reduce(
@@ -8,3 +11,24 @@ export const processForm = (elements) =>
       }),
       {}
     );
+
+export const createCurrentClients = (data) => {
+  tbody.innerHTML = null;
+
+  data.forEach(({ clientNumber, ownerName, cell, petName }) => {
+    const newClientRow = template.content.cloneNode(true);
+    const newClientTDs = newClientRow.querySelectorAll("td");
+
+    newClientTDs[0].textContent = clientNumber;
+    newClientTDs[1].textContent = ownerName;
+    newClientTDs[2].textContent = cell;
+    newClientTDs[3].textContent = petName;
+
+    tbody.appendChild(newClientRow);
+  });
+};
+
+export const clientWithNumber = (elements, data) => ({
+  id: data.count(),
+  ...processForm(elements),
+});
