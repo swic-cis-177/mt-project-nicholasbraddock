ddfa928c1e5717d57b0f4290adfdf3b589d8886e
Nicholas Braddock <nicholas.braddock@student.swic.edu>
2020-12-07 15:40:09 -0600

    added first client to data list

---
+++ data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    ownerName: "Nick Braddock",
+    email: "nicholas.braddock@student.swic.edu",
+    cell: "1234567890",
+    home: "1234567890",
+    street: "123 First Street",
+    city: "O'Fallon",
+    state: "IL",
+    zip: "62269",
+    petName: "Rudy",
+    dob: "2019-02-09",
+    species: "Dog",
+    breed: "Poodle",
+    color: "Red",
+    sex: "Male",
+    sexuallyIntact: "No, Neutered",
+    shotsCurrent: "Yes",
+  },
+];
