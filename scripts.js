c4d009e168d2c5215fb1bf6cc9d46eb8ed786163
Nicholas Braddock <nicholas.braddock@student.swic.edu>
2020-12-06 19:06:42 -0600

    Editted scripts to remove the process form

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
+import { processForm } from "./lib.js";
 const form = document.querySelector("form");
 
 form.addEventListener("submit", function () {
   event.preventDefault();
 
-  const formDataHuman = {};
-  for (let i = 0; i <= 15; i++) {
-    formDataHuman[event.target.elements[i].id] = event.target.elements[i].value;
-  }
-
-  console.log(formDataHuman);
+  processForm(event.target.elements);
 });
